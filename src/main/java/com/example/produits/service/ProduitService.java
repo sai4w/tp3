6495ac50da305fc7ed936bd6099e99d74126c64b
saif zhioua <seif.zhioua@gmail.com>
2022-04-14 23:34:26 +0100

    init

---
+++ src/main/java/com/example/produits/service/ProduitService.java
@@ -0,0 +1,22 @@
+package com.moulouk.produits.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.moulouk.produits.entities.Produit;
+
+public interface ProduitService {
+
+	Produit saveProduit(Produit p);
+	Produit updateProduit(Produit p);
+	void deleteProduit(Produit p);
+	void deleteProduitById(Long id);
+	Produit getProduit(Long id);
+	List<Produit> getAllProduits();
+	
+	
+	
+	
+	Page<Produit> getAllProduitsParPage(int page, int size);
+}
